13693d184f843dc6a3262b567c3a48b2e3870c68
Luca Meyer <luca.meyer@he-arc.ch>
2021-12-02 15:01:31 +0100

    Added new relation between users and Products

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,14 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+         * Get all of the products for the User
+         *
+         * @return \Illuminate\Database\Eloquent\Relations\HasManyThrough
+         */
+        public function products(): HasManyThrough
+        {
+            return $this->hasManyThrough(Product::class, UserHas::class);
+        }
 }
